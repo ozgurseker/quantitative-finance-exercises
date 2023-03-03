ae54b1f761ff8fa431ff4c58d5a412d6ed21588e
ozgurseker <ozgursheker@gmail.com>
2023-03-03 17:04:17 -0500

    black scholes

---
+++ black-scholes/black_scholes.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  3 16:11:34 2023
+
+Implementing the black-scholes formula in python
+
+@author: ozgur
+"""
+
+import numpy as np
+from scipy.stats import norm
+
